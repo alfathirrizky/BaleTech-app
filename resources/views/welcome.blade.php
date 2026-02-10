4904c794e535a5c650f9d49a228cb22033f3e7a2
alfathir rizky harsya <alfathirrizky083@gmail.com>
2026-02-10 20:05:54 +0700

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
             </div>
         </div>
     </section>
-    <section id="services" class="flex items-center h-screen">
+    <section id="services" class="flex items-center justify-center p-30 bg-gray-50">
         <div class="max-w-7xl mx-auto px-6 flex flex-col w-full gap-10">
             <div>
                 <h2 class="text-4xl font-bold text-center">Our Services</h2>
@@ -30,31 +30,73 @@
             <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
                 <div class="bg-white p-6 rounded-2xl shadow hover:shadow-lg transition-shadow duration-300">
                     <h3 class="text-xl font-bold mb-2">Web Development</h3>
-                    <p class="">Custom web applications tailored to your business needs.</p>
-                    <ul class="text-slate-600">
+                    <p class="text-slate-800">Custom web applications tailored to your business needs.</p>
+                    <ul class="text-slate-600 text-sm">
+                        <li class="mt-2">• Responsive Design</li>
+                        <li class="mt-2">• E-commerce Solutions</li>
+                        <li class="mt-2">• CMS Integration</li>
+                        <li class="mt-2">• Responsive Design</li>
+                        <li class="mt-2">• E-commerce Solutions</li>
+                        <li class="mt-2">• CMS Integration</li>
                         <li class="mt-2">• Responsive Design</li>
                         <li class="mt-2">• E-commerce Solutions</li>
                         <li class="mt-2">• CMS Integration</li>
                     </ul>
+                    <h2 class="text-lg font-bold mt-4">Start from Rp 300.000</h2>
+                    <button class="bg-blue-600 hover:bg-blue-300 text-white font-semibold py-2 px-4 rounded-2xl text-xs mt-1 cursor-pointer">Learn More</button>
                 </div>
                 <div class="bg-white p-6 rounded-2xl shadow hover:shadow-lg transition-shadow duration-300">
                     <h3 class="text-xl font-bold mb-2">Mobile Apps</h3>
-                    <p>Native and cross-platform mobile applications for iOS and Android.</p>
-                    <ul class="text-slate-600">
+                    <p class="text-slate-800">Native and cross-platform mobile applications for iOS and Android.</p>
+                    <ul class="text-slate-600 text-sm">
+                        <li class="mt-2">• User-friendly Interfaces</li>
+                        <li class="mt-2">• Performance Optimization</li>
+                        <li class="mt-2">• Regular Updates</li>
+                        <li class="mt-2">• User-friendly Interfaces</li>
+                        <li class="mt-2">• Performance Optimization</li>
+                        <li class="mt-2">• Regular Updates</li>
                         <li class="mt-2">• User-friendly Interfaces</li>
                         <li class="mt-2">• Performance Optimization</li>
                         <li class="mt-2">• Regular Updates</li>
                     </ul>
+                    <h2 class="text-lg font-bold mt-4">Start from Rp 200.000</h2>
+                    <button class="bg-blue-600 hover:bg-blue-300 text-white font-semibold py-2 px-4 rounded-2xl text-xs mt-1 cursor-pointer">Learn More</button>
                 </div>
                 <div class="bg-white p-6 rounded-2xl shadow hover:shadow-lg transition-shadow duration-300">
                     <h3 class="text-xl font-bold mb-2">UI/UX Design</h3>
-                    <p>User-centered design that enhances user experience and engagement.</p>
-                    <ul class="text-slate-600">
+                    <p class="text-slate-800">User-centered design that enhances user experience and engagement.</p>
+                    <ul class="text-slate-600 text-sm">
+                        <li class="mt-2">• Wireframing & Prototyping</li>
+                        <li class="mt-2">• Visual Design</li>
+                        <li class="mt-2">• Usability Testing</li>
+                        <li class="mt-2">• Wireframing & Prototyping</li>
+                        <li class="mt-2">• Visual Design</li>
+                        <li class="mt-2">• Usability Testing</li>
                         <li class="mt-2">• Wireframing & Prototyping</li>
                         <li class="mt-2">• Visual Design</li>
                         <li class="mt-2">• Usability Testing</li>
                     </ul>
+                    <h2 class="text-lg font-bold mt-4">Start from Rp 100.000</h2>
+                    <button class="bg-blue-600 hover:bg-blue-300 text-white font-semibold py-2 px-4 rounded-2xl text-xs mt-1 cursor-pointer">Learn More</button>
                 </div>
+            </div>
+        </div>
+    </section>
+    <section class="flex justify-center items-center p-10" id="contact">
+        <div class="flex flex-col w-5xl h-96 justify-center items-center bg-[url(/img/bg.jpg)] bg-center bg-cover m-10 rounded-2xl shadow-md" style="background-image: url('{{ asset('img/bg.jpg') }}')">
+            <h1 class="text-white text-3xl font-bold">contact us</h1>
+            <p class="text-white text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt, dolor.</p>
+            <div class="flex items-center justify-center gap-2 border border-white/20 rounded-4xl bg-white/30 backdrop-blur-md p-3 shadow-lg mt-4">
+                <input 
+                type="email" 
+                placeholder="Your Email....." 
+                class="px-4 py-2 rounded-2xl w-80 text-sm text-white focus:outline-none"
+                >
+                <button 
+                class="bg-blue-600 hover:bg-blue-300 transition duration-300 text-white font-semibold py-2 px-4 rounded-2xl text-sm cursor-pointer"
+                >
+                Subscribe
+                </button>
             </div>
         </div>
     </section>
